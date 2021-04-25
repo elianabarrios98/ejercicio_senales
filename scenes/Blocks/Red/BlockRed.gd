464bde0dcb196fcb1863104bc434e6797985027e
Eliana Barrios <elianabarrios9827@gmail.com>
2021-04-25 20:47:24 -0300

    funcion_delete
    En los scripts creados para "BlockRed" "BlockViolet" se agregó la función llamada "delete".

--- scenes/Blocks/Red/BlockRed.gd
+++ scenes/Blocks/Red/BlockRed.gd
@@ -3,3 +3,9 @@ extends RigidBody2D
 func _ready():
 	add_to_group("blocks")
 	pass
+	
+func delete():
+	var cantidadBloques = .get_child_count()
+	if cantidadBloques > 0:
+		get_child(cantidadBloques).queue_free()
+	pass
